deff6cb497d35de4e6c87a282896b2ffdb767175
John Paul Antonovich <AntonovichJP@gmail.com>
2019-02-09 16:05:57 -0800

    Added more stuff

---
+++ Assets/Scenes/Scripts/timer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class timer : MonoBehaviour
+{
+    public Text timerText;
+    private float time = 0f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        timerText.text = time.ToString("0");   
+    }
+}
