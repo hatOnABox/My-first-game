715118ba910a80334387548f988fa13251cbc766
John Paul Antonovich <AntonovichJP@gmail.com>
2019-02-10 15:55:59 -0800

    Added credits menu

---
+++ Assets/Scenes/Scripts/QuitGame.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class QuitGame : MonoBehaviour
+{
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+}
--- Assets/Scenes/Scripts/winLevel.cs
+++ Assets/Scenes/Scripts/winLevel.cs
@@ -8,7 +8,7 @@ public class winLevel : MonoBehaviour
         if (collision.collider.name == "Player")
         {
             //FindObjectOfType<GameManagerScript>().reload_game();
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
         }
     }
 }
